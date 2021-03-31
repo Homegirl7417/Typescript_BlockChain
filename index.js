0ad2dfa586eb4f08bd951369d007608be1bd4b79
ParkHyunKyeong <kj3477417@gmail.com>
2021-03-31 14:36:59 +0900

    save files

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "HyunKyeong Park", age = 24, gender = 'female';
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are a ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("hello");
+const name = "HyunKyeong Park",
+    age = 24,
+    gender = 'female';
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are a ${age}, you are a ${gender}`);
+}
+
+sayHi(name, age);
+export {};
