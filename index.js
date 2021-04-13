c5091d92de931521af75dd6bbcd0998d96545a01
ParkHyunKyeong <kj3477417@gmail.com>
2021-04-13 12:39:20 +0900

    after: install tsc-watch

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "HyunKyeong Park", age = 24, gender = 'female';
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are a ${age}, you are a ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
